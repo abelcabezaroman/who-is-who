f8aae33849818452fe8fc3ca588b3476686e4d74
acabezar <contacto@abelcabezaroman.com>
2021-02-22 16:43:12 +0100

    init

---
+++ main.js
@@ -0,0 +1,574 @@
+const questionsType = [
+    {
+        title: 'Gender',
+        key: 'gender',
+        questions: ['Man', 'Woman']
+    },
+    {
+        title: 'Hair Color',
+        key: 'hairColor',
+        questions: ['Blonde', 'Red', 'Pink', 'Brown', 'White', 'Black']
+    },
+    {
+        title: 'Moustache',
+        key: 'moustache',
+        questions: ['Yes', 'No']
+    },
+    {
+        title: 'Glasses',
+        key: 'glasses',
+        questions: ['Yes', 'No']
+    },
+    {
+        title: 'Hat or Cap',
+        key: 'hatOrCap',
+        questions: ['Yes', 'No']
+    },
+    {
+        title: 'Clothes color',
+        key: 'clothesColor',
+        questions: ['Red', 'Orange', 'Green', 'White', 'Black', 'Pink', 'Red']
+    },
+    {
+        title: 'Skin color',
+        key: 'skinColor',
+        questions: ['Light', 'Dark']
+    },
+    {
+        title: 'Long Hair',
+        key: 'longHair',
+        questions: ['Yes', 'No']
+    }
+]
+
+const persons = [
+    {
+        img: 'public/001-man.svg',
+        gender: 'Man',
+        hairColor: 'Black',
+        moustache: 'No',
+        glasses: 'No',
+        hatOrCap: 'No',
+        clothesColor: 'Red',
+        skinColor: 'Light',
+        longHair: 'No'
+    }, {
+        img: 'public/002-man.svg',
+        gender: 'Man',
+        hairColor: 'Blonde',
+        moustache: 'No',
+        glasses: 'No',
+        hatOrCap: 'No',
+        clothesColor: 'Orange',
+        skinColor: 'Light',
+        longHair: 'No'
+    }, {
+        img: 'public/003-man.svg',
+        gender: 'Man',
+        hairColor: 'Black',
+        moustache: 'No',
+        glasses: 'No',
+        hatOrCap: 'No',
+        clothesColor: 'Green',
+        skinColor: 'Light',
+        longHair: 'No'
+    }, {
+        img: 'public/004-man.svg',
+        gender: 'Man',
+        hairColor: 'Blonde',
+        moustache: 'No',
+        glasses: 'No',
+        hatOrCap: 'Yes',
+        clothesColor: 'Red',
+        skinColor: 'Light',
+        longHair: 'No'
+    }, {
+        img: 'public/005-man.svg',
+        gender: 'Man',
+        hairColor: 'Black',
+        moustache: 'No',
+        glasses: 'No',
+        hatOrCap: 'No',
+        clothesColor: 'White',
+        skinColor: 'Light',
+        longHair: 'No'
+    }, {
+        img: 'public/006-man.svg',
+        gender: 'Man',
+        hairColor: 'Brown',
+        moustache: 'Yes',
+        glasses: 'No',
+        hatOrCap: 'No',
+        clothesColor: 'Green',
+        skinColor: 'Light',
+        longHair: 'No'
+    }, {
+        img: 'public/007-man.svg',
+        gender: 'Man',
+        hairColor: 'Red',
+        moustache: 'No',
+        glasses: 'No',
+        hatOrCap: 'No',
+        clothesColor: 'Black',
+        skinColor: 'Light',
+        longHair: 'No'
+    }, {
+        img: 'public/008-man.svg',
+        gender: 'Man',
+        hairColor: 'Black',
+        moustache: 'No',
+        glasses: 'No',
+        hatOrCap: 'No',
+        clothesColor: 'White',
+        skinColor: 'Light',
+        longHair: 'No'
+    }, {
+        img: 'public/009-man.svg',
+        gender: 'Man',
+        hairColor: 'Blonde',
+        moustache: 'No',
+        glasses: 'No',
+        hatOrCap: 'No',
+        clothesColor: 'Orange',
+        skinColor: 'Light',
+        longHair: 'No'
+    }, {
+        img: 'public/010-woman.svg',
+        gender: 'Woman',
+        hairColor: 'Brown',
+        moustache: 'No',
+        glasses: 'No',
+        hatOrCap: 'No',
+        clothesColor: 'Pink',
+        skinColor: 'Light',
+        longHair: 'Yes'
+    }, {
+        img: 'public/011-woman.svg',
+        gender: 'Woman',
+        hairColor: 'Black',
+        moustache: 'No',
+        glasses: 'No',
+        hatOrCap: 'No',
+        clothesColor: 'Orange',
+        skinColor: 'Light',
+        longHair: 'Yes'
+    }, {
+        img: 'public/012-woman.svg',
+        gender: 'Woman',
+        hairColor: 'Red',
+        moustache: 'No',
+        glasses: 'No',
+        hatOrCap: 'No',
+        clothesColor: 'Black',
+        skinColor: 'Light',
+        longHair: 'Yes'
+    }, {
+        img: 'public/013-woman.svg',
+        gender: 'Woman',
+        hairColor: 'White',
+        moustache: 'No',
+        glasses: 'No',
+        hatOrCap: 'No',
+        clothesColor: 'Pink',
+        skinColor: 'Light',
+        longHair: 'Yes'
+    }, {
+        img: 'public/014-woman.svg',
+        gender: 'Woman',
+        hairColor: 'Black',
+        moustache: 'No',
+        glasses: 'No',
+        hatOrCap: 'No',
+        clothesColor: 'White',
+        skinColor: 'Light',
+        longHair: 'Yes'
+    }, {
+        img: 'public/015-woman.svg',
+        gender: 'Woman',
+        hairColor: 'Brown',
+        moustache: 'No',
+        glasses: 'Yes',
+        hatOrCap: 'No',
+        clothesColor: 'Orange',
+        skinColor: 'Light',
+        longHair: 'Yes'
+    }, {
+        img: 'public/016-woman.svg',
+        gender: 'Woman',
+        hairColor: 'Black',
+        moustache: 'No',
+        glasses: 'No',
+        hatOrCap: 'Yes',
+        clothesColor: 'Pink',
+        skinColor: 'Light',
+        longHair: 'Yes'
+    }, {
+        img: 'public/017-woman.svg',
+        gender: 'Woman',
+        hairColor: 'Blonde',
+        moustache: 'No',
+        glasses: 'No',
+        hatOrCap: 'No',
+        clothesColor: 'Black',
+        skinColor: 'Light',
+        longHair: 'No'
+    }, {
+        img: 'public/018-man.svg',
+        gender: 'Man',
+        hairColor: 'Black',
+        moustache: 'No',
+        glasses: 'No',
+        hatOrCap: 'No',
+        clothesColor: 'White',
+        skinColor: 'Dark',
+        longHair: 'No'
+    }, {
+        img: 'public/019-man.svg',
+        gender: 'Man',
+        hairColor: 'Black',
+        moustache: 'No',
+        glasses: 'No',
+        hatOrCap: 'No',
+        clothesColor: 'Red',
+        skinColor: 'Dark',
+        longHair: 'No'
+    }, {
+        img: 'public/020-man.svg',
+        gender: 'Man',
+        hairColor: 'Blonde',
+        moustache: 'No',
+        glasses: 'No',
+        hatOrCap: 'No',
+        clothesColor: 'Black',
+        skinColor: 'Dark',
+        longHair: 'No'
+    }, {
+        img: 'public/021-man.svg',
+        gender: 'Man',
+        hairColor: 'Black',
+        moustache: 'Yes',
+        glasses: 'No',
+        hatOrCap: 'No',
+        clothesColor: 'Orange',
+        skinColor: 'Dark',
+        longHair: 'No'
+    }, {
+        img: 'public/022-man.svg',
+        gender: 'Man',
+        hairColor: 'Black',
+        moustache: 'No',
+        glasses: 'Yes',
+        hatOrCap: 'No',
+        clothesColor: 'Black',
+        skinColor: 'Dark',
+        longHair: 'No'
+    }, {
+        img: 'public/023-man.svg',
+        gender: 'Man',
+        hairColor: 'Black',
+        moustache: 'No',
+        glasses: 'No',
+        hatOrCap: 'No',
+        clothesColor: 'Orange',
+        skinColor: 'Dark',
+        longHair: 'No'
+    }, {
+        img: 'public/024-man.svg',
+        gender: 'Man',
+        hairColor: 'Blonde',
+        moustache: 'No',
+        glasses: 'No',
+        hatOrCap: 'No',
+        clothesColor: 'Black',
+        skinColor: 'Dark',
+        longHair: 'Yes'
+    }, {
+        img: 'public/025-man.svg',
+        gender: 'Man',
+        hairColor: 'Black',
+        moustache: 'No',
+        glasses: 'No',
+        hatOrCap: 'No',
+        clothesColor: 'Red',
+        skinColor: 'Dark',
+        longHair: 'No'
+    }, {
+        img: 'public/026-man.svg',
+        gender: 'Man',
+        hairColor: 'Black',
+        moustache: 'No',
+        glasses: 'No',
+        hatOrCap: 'No',
+        clothesColor: 'Red',
+        skinColor: 'Dark',
+        longHair: 'No'
+    }, {
+        img: 'public/027-woman.svg',
+        gender: 'Woman',
+        hairColor: 'Black',
+        moustache: 'No',
+        glasses: 'No',
+        hatOrCap: 'No',
+        clothesColor: 'Orange',
+        skinColor: 'Dark',
+        longHair: 'Yes'
+    }, {
+        img: 'public/028-woman.svg',
+        gender: 'Woman',
+        hairColor: 'Black',
+        moustache: 'No',
+        glasses: 'No',
+        hatOrCap: 'No',
+        clothesColor: 'Pink',
+        skinColor: 'Dark',
+        longHair: 'Yes'
+    }, {
+        img: 'public/029-woman.svg',
+        gender: 'Woman',
+        hairColor: 'Black',
+        moustache: 'No',
+        glasses: 'No',
+        hatOrCap: 'No',
+        clothesColor: 'White',
+        skinColor: 'Dark',
+        longHair: 'Yes'
+    }, {
+        img: 'public/030-woman.svg',
+        gender: 'Woman',
+        hairColor: 'Blonde',
+        moustache: 'No',
+        glasses: 'No',
+        hatOrCap: 'No',
+        clothesColor: 'Orange',
+        skinColor: 'Dark',
+        longHair: 'No'
+    }, {
+        img: 'public/031-woman.svg',
+        gender: 'Woman',
+        hairColor: 'Black',
+        moustache: 'No',
+        glasses: 'No',
+        hatOrCap: 'No',
+        clothesColor: 'Green',
+        skinColor: 'Dark',
+        longHair: 'Yes'
+    }, {
+        img: 'public/032-woman.svg',
+        gender: 'Woman',
+        hairColor: 'Black',
+        moustache: 'No',
+        glasses: 'Yes',
+        hatOrCap: 'No',
+        clothesColor: 'Pink',
+        skinColor: 'Dark',
+        longHair: 'Yes'
+    }, {
+        img: 'public/033-woman.svg',
+        gender: 'Woman',
+        hairColor: 'Black',
+        moustache: 'No',
+        glasses: 'No',
+        hatOrCap: 'No',
+        clothesColor: 'Black',
+        skinColor: 'Dark',
+        longHair: 'Yes'
+    }, {
+        img: 'public/034-woman.svg',
+        gender: 'Woman',
+        hairColor: 'Black',
+        moustache: 'No',
+        glasses: 'No',
+        hatOrCap: 'No',
+        clothesColor: 'Pink',
+        skinColor: 'Dark',
+        longHair: 'Yes'
+    }, {
+        img: 'public/035-man.svg',
+        gender: 'Man',
+        hairColor: 'Black',
+        moustache: 'No',
+        glasses: 'No',
+        hatOrCap: 'No',
+        clothesColor: 'Black',
+        skinColor: 'Light',
+        longHair: 'No'
+    }, {
+        img: 'public/036-man.svg',
+        gender: 'Man',
+        hairColor: 'Black',
+        moustache: 'No',
+        glasses: 'No',
+        hatOrCap: 'No',
+        clothesColor: 'White',
+        skinColor: 'Light',
+        longHair: 'No'
+    }, {
+        img: 'public/037-man.svg',
+        gender: 'Man',
+        hairColor: 'Black',
+        moustache: 'No',
+        glasses: 'No',
+        hatOrCap: 'Yes',
+        clothesColor: 'Red',
+        skinColor: 'Light',
+        longHair: 'No'
+    }, {
+        img: 'public/038-man.svg',
+        gender: 'Man',
+        hairColor: 'Blonde',
+        moustache: 'No',
+        glasses: 'No',
+        hatOrCap: 'No',
+        clothesColor: 'Black',
+        skinColor: 'Light',
+        longHair: 'No'
+    }, {
+        img: 'public/039-man.svg',
+        gender: 'Man',
+        hairColor: 'Black',
+        moustache: 'Yes',
+        glasses: 'No',
+        hatOrCap: 'Yes',
+        clothesColor: 'Green',
+        skinColor: 'Light',
+        longHair: 'No'
+    }, {
+        img: 'public/040-man.svg',
+        gender: 'Man',
+        hairColor: 'Black',
+        moustache: 'No',
+        glasses: 'No',
+        hatOrCap: 'No',
+        clothesColor: 'Red',
+        skinColor: 'Light',
+        longHair: 'No'
+    }, {
+        img: 'public/041-man.svg',
+        gender: 'Man',
+        hairColor: 'Blonde',
+        moustache: 'No',
+        glasses: 'No',
+        hatOrCap: 'No',
+        clothesColor: 'Black',
+        skinColor: 'Light',
+        longHair: 'No'
+    }, {
+        img: 'public/042-man.svg',
+        gender: 'Man',
+        hairColor: 'Black',
+        moustache: 'No',
+        glasses: 'No',
+        hatOrCap: 'No',
+        clothesColor: 'Red',
+        skinColor: 'Light',
+        longHair: 'No'
+    }, {
+        img: 'public/043-woman.svg',
+        gender: 'Woman',
+        hairColor: 'Black',
+        moustache: 'No',
+        glasses: 'No',
+        hatOrCap: 'No',
+        clothesColor: 'Black',
+        skinColor: 'Light',
+        longHair: 'Yes'
+    }, {
+        img: 'public/044-woman.svg',
+        gender: 'Woman',
+        hairColor: 'Black',
+        moustache: 'No',
+        glasses: 'No',
+        hatOrCap: 'No',
+        clothesColor: 'Pink',
+        skinColor: 'Light',
+        longHair: 'No'
+    }, {
+        img: 'public/045-woman.svg',
+        gender: 'Woman',
+        hairColor: 'Black',
+        moustache: 'No',
+        glasses: 'No',
+        hatOrCap: 'No',
+        clothesColor: 'White',
+        skinColor: 'Light',
+        longHair: 'No'
+    }
+]
+
+const boardGame$$ = document.querySelector('[data-function="boardGame"]');
+const questions$$ = document.querySelector('[data-function="questions"]');
+const clueCount$$ = document.querySelector('[data-function="clueCount"]');
+let persons$$;
+let personToWin;
+
+startGame()
+
+function createQuestions () {
+    questions$$.innerHTML = '';
+    for (const questionType of questionsType) {
+        const div$$ = document.createElement('div');
+        div$$.innerHTML = `<h2>${questionType.title}</h2>`
+        for (const question of questionType.questions) {
+            const button$$ = document.createElement('button');
+            button$$.addEventListener('click', () => {
+                disableHtmlNode(button$$);
+                clickedAnswer(questionType.key, question);
+            });
+            button$$.textContent = question;
+            div$$.appendChild(button$$);
+        }
+        questions$$.appendChild(div$$);
+    }
+}
+
+function createBoard () {
+    boardGame$$.innerHTML = '';
+    for (const person of persons) {
+        const img$$ = document.createElement('img');
+        img$$.setAttribute('src', person.img);
+        img$$.addEventListener('click', () => {checkFinalAnswer(person)})
+        persons$$.push(img$$);
+        boardGame$$.appendChild(img$$)
+    }
+}
+
+function clickedAnswer (questionKey, question) {
+    clueCount$$.textContent = Number(clueCount$$.textContent) + 1;
+    const answer = personToWin[questionKey] === question;
+    for (let i = 0; i < persons.length; i++) {
+        let person = persons[i];
+        if ((answer && person[questionKey] !== question) || (!answer && person[questionKey] === question)) {
+            disableHtmlNode(persons$$[i]);
+        }
+    }
+}
+
+function disableHtmlNode (node) {
+    node.style.opacity = '0.2';
+    node.style.pointerEvents = 'none';
+}
+
+function randomPerson () {
+    const randomNumber = Math.floor(Math.random() * persons.length);     // returns a random integer from 0 to 9
+    personToWin = persons[randomNumber];
+}
+
+function checkFinalAnswer(person){
+    if(personToWin === person){
+        alert('Congratulations! You win game with ' + clueCount$$.textContent + ' clues!')
+    } else{
+        alert('Sorry! You lose game! :(')
+    }
+
+    startGame();
+
+}
+
+
+function startGame () {
+    clueCount$$.textContent = '0';
+    persons$$ = [];
+    randomPerson();
+    createQuestions();
+    createBoard();
+}
